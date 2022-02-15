4c51110c2f9ecfad647bf69353369c4db5d0d4e1
Gustavo Seiti <endo.tec@live.com>
2022-02-14 23:40:21 -0300

    Instalado express e adicionado index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, () => console.log("O servidor está rodando na porta 3000!"));
+
+app.get("/atendimentos", (req, res) => {
+    res.send("Você está na rota de atendimentos.")
+})
