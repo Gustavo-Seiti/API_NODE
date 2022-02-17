e41f0498e1c08f7de0f2a987240a1b3b10d51726
Gustavo Seiti <endo.tec@live.com>
2022-02-17 00:48:25 -0300

    s

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -4,10 +4,12 @@ const mysql = require('mysql');
 const conexao = mysql.createConnection({
     host: 'us-cdbr-east-05.cleardb.net',
     port: 3306,
-    user: 'b53939ab8565a7',
-    password: '27bc2914',
-    database: 'us-cdbr-east-05.cleardb.net?'
+    user: '	b4e47e428ac57f',
+    password: '2bbcacf9',
+    database: 'heroku_0ee5d952cb73cc4'
 })
 
 module.exports = conexao;
+
+
 
