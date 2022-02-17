74ba65b96adffdb3745208b9b4a7266e5701cde2
Gustavo Seiti <endo.tec@live.com>
2022-02-17 00:51:07 -0300

    Alteração de user

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const conexao = mysql.createConnection({
     host: 'us-cdbr-east-05.cleardb.net',
     port: 3306,
-    user: '	b4e47e428ac57f',
+    user: 'b4e47e428ac57f',
     password: '2bbcacf9',
     database: 'heroku_0ee5d952cb73cc4'
 })
