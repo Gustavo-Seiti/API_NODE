ebd71956fab23a617b6374231c14fa1c00eb4f30
Gustavo Seiti <endo.tec@live.com>
2022-02-17 23:00:42 -0300

    Correção BD

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 
 
 const conexao = mysql.createConnection({
-    host: 'us-cdbr-east-05.cleardb.net',
+    host: 'localhost',
     port: 3306,
-    user: 'b4e47e428ac57f',
-    password: '2bbcacf9',
-    database: 'heroku_0ee5d952cb73cc4'
+    user: 'root',
+    password: '9608',
+    database: 'agenda-petshop'
 })
 
 module.exports = conexao;
